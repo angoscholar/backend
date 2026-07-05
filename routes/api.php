33466b07f4d3895fa01cdeb828c0385bce0acb10
angoscholar <angoscholar@gmail.com>
2026-07-05 19:36:29 +0100

    Add ping route for API status check

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/ping', function () {
+    return response()->json([
+        'status' => 'ok',
+        'message' => 'API do AngoScholar funcionando!'
+    ]);
+});
