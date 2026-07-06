dcac2c12d0a43292407168290f0e3602aef90035
angoscholar <angoscholar@gmail.com>
2026-07-06 07:40:14 +0100

    Create cors.php

---
+++ config/cors.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '*')),
+    'allowed_origins_patterns' => [
+        '#^https?://.*\.vercel\.app$#',
+        '#^https?://localhost(:\d+)?$#',
+    ],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
+];
